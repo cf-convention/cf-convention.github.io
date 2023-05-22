59f79b2afc302dc0a00f18dfc2a9192db4b8e578
japamment <alison.pamment@stfc.ac.uk>
2023-05-22 02:32:36 +0100

    Update standard names processing to Python 3.

--- Data/cf-standard-names/81/inject-area-type-link.py
+++ Data/cf-standard-names/81/inject-area-type-link.py
@@ -5,15 +5,14 @@ import sys, re
 HELP = """%s - Injects link to area_type table. \nUsage:  %s <file>"""%(sys.argv[0], sys.argv[0])
 
 if len(sys.argv) < 2:
-    print HELP
+    print(HELP)
     sys.exit(1)
     
 file = sys.argv[1]
 
 # Read input.
-i = open(file, 'r')
-s = i.read()
-i.close()
+with open(file, 'r', encoding="utf8") as i:
+    s = i.read()
     
 # Replace text.
 text = 'area_type table'
@@ -21,11 +20,10 @@ p = re.compile(text, re.DOTALL)
 fix = re.sub(p, '<a href="http://cfconventions.org/Data/area-type-table/current/build/area-type-table.html">%s</a>'%text, s, 1)
 
 # Write output.
-o = open(file, 'w')
-o.write(fix)
-o.close()
+with open(file, 'w', encoding="utf8", newline="\n") as o:
+    o.write(fix)
 
-print '%s - injected link'%file
+print('%s - injected link'%file)
     
 
 
